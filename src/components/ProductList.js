7983c941901000104a165fc2590991dfb6309861
Mawuli87 <yawomessie@gmail.com>
2024-08-18 17:03:06 +0000

    Calling useEffect in a function

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,9 +6,12 @@ export const ProductList = () => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
+    const fetchProducts = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+    };
+    fetchProducts();
   }, [url]);
 
   useEffect(() => {
